ceb514d79e45f2475d9f67c11604a4c4980d08b8
misaelpc <misaelpcgm@gmail.com>
2015-08-26 10:00:35 -0500

    Initial Commit

---
+++ VectorIcons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VectorIcons
+//
+//  Created by Misael Pérez Chamorro on 8/26/15.
+//  Copyright (c) 2015 Raku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ VectorIconsTests/VectorIconsTests.swift
@@ -0,0 +1,36 @@
+//
+//  VectorIconsTests.swift
+//  VectorIconsTests
+//
+//  Created by Misael Pérez Chamorro on 8/26/15.
+//  Copyright (c) 2015 Raku. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class VectorIconsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
